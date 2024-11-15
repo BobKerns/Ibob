cbd671cfa0476808d8e9755f58f470b32f704cbb
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2024-11-15 14:21:37 -0800

    Testing git-pwd, we can't rely on  chdir to update the context.

--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -3,26 +3,25 @@ Test the XGit commands.
 '''
 
 from pathlib import Path
-from typing import MutableMapping
+from typing import Any, MutableMapping
 
 from xonsh.built_ins import XonshSession
 
-
-def test_pwd(with_xgit, capsys, chdir):
+def test_pwd_no_repo(with_xgit, capsys, chdir):
     '''
     Test the xsh proxy.
     '''
-    def _t(*_, XSH: XonshSession, git_pwd, **__) -> None:
+    def _t(*_, XSH: XonshSession, git_pwd, XGIT, target, **__) -> None:
         root = Path('/').resolve()
         chdir(root)
+        target(XGIT, None)
         git_pwd()
         output = capsys.readouterr()
         env = XSH.env
         assert isinstance(env, MutableMapping), \
             f"XSH.env not a MutableMapping {env!r}"
-
         lines = output.out.strip().split('\n')
         assert len(lines) == 2, f"Expected 2 lines, got {len(lines)}: {lines}"
         assert lines[0] == f'cwd: {root}'
         assert lines[1] == 'Not in a git repository'
-    with_xgit(_t)
+    with_xgit(_t, 'xontrib.xgit.proxy','xontrib.xgit.vars')
