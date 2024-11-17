5c73f98c79091af9241b55a9c5b134ff4fa3209e
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2024-11-17 10:15:31 -0800

    Adjust context json and tests.

--- tests/test_context.py
+++ tests/test_context.py
@@ -2,10 +2,10 @@
 Tests for `GitContext`.
 '''
 
-from typing import AnyStr, cast, Any
+from typing import Any
 from unittest.mock import NonCallableMock
+from pathlib import Path
 
-_GitContext: Any
 to_json: Any
 from_json: Any
 
@@ -14,26 +14,33 @@ def test_context_loads(modules, sysdisplayhook):
     with modules('xontrib.xgit.context', 'xontrib.xgit.vars') as ((m_ctx, _), vars):
         assert m_ctx is not None
 
-def test_context_simple(with_xgit, sysdisplayhook):
+def test_context_simple(with_xgit, worktree, sysdisplayhook):
     def _t(*_, _GitContext, **__):
-        ctx = _GitContext()
+        ctx = _GitContext(worktree=worktree)
         assert ctx is not NonCallableMock
     with_xgit(_t, 'xontrib.xgit.context', 'xontrib.xgit.vars')
 
-def test_context_json(with_xgit, sysdisplayhook, test_branch):
+def test_context_json(with_xgit, worktree, git, sysdisplayhook, test_branch):
     def _t(*_, _GitContext, to_json, from_json, _run_text, **__):
-        ref = f'refs/heads/{test_branch}'
-        head = _run_text(['git', 'rev-parse', 'HEAD'])
-        ctx = _GitContext()
-        ctx.commit = head
-        ctx.branch = ref
+        head = git('rev-parse', 'HEAD', cwd=worktree.path)
+        branch = git('symbolic-ref', 'HEAD', cwd=worktree.path)
+        ctx = _GitContext(worktree=worktree)
         j = to_json(ctx, _GitContext)
-        assert j == {
-            'branch': ref,
-            'common': '.git',
-            'git_path': '.',
+        path = worktree.path
+        expected = {
+            'branch': branch,
+            'path': '.',
             'commit': head,
-            'worktree': '.',
-            'repository': '.git'
+            'worktree': {
+                "repository": str(path / '.git'),
+                'path': str(path),
+                'repository_path': str(path / '.git'),
+                'branch': branch,
+                'commit': head,
+                'locked': '',
+                'prunable': '',
+            },
         }
+        assert j['worktree'] == expected['worktree']
+        assert j == expected
     with_xgit(_t, 'xontrib.xgit.context', 'xontrib.xgit.to_json', 'xontrib.xgit.vars')
